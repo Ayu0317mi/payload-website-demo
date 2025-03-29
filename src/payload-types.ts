e4ff41df1d4770fb64e388b54a00fa46072bbdb8
Ayu0317mi <n0217ayumi@gmail.com>
2025-03-29 13:51:24 +1000

    Create MyHero components on hero section

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -149,7 +149,7 @@ export interface Page {
   id: string;
   title: string;
   hero: {
-    type: 'none' | 'highImpact' | 'mediumImpact' | 'lowImpact';
+    type: 'none' | 'highImpact' | 'mediumImpact' | 'lowImpact' | 'myHero';
     richText?: {
       root: {
         type: string;
